209281198fea8bab48b6ba12bbc95c314ef05572
li886580 <li886580@gmail.com>
2021-02-16 16:34:03 +0800

    first

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,26 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <to-do-list/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ToDoList from './components/to-do-list.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    ToDoList
   }
 }
 </script>
 
 <style>
+@import './assets/css/vendors/CSS Reset.css';
+html, body {
+  height: 100%;
+}
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  height: 100%;
 }
 </style>
---
+++ src/components/to-do-list.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="content">
+    <div class="header">
+      <p>To Do List</p>
+      <input type="text" placeholder="新增清單..">
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style src='./to-do-list.css'>
+</style>
